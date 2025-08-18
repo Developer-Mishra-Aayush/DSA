7c82e5a0ff6f9ccb24d359a6b554cd25851841e0
Aayush Mishra <ayush28014@gmail.com>
2025-08-18 11:16:50 +0530

    Sort Colors

---
+++ Arrays/sort_0_1_2.py
@@ -0,0 +1,24 @@
+def sort_0_1_2(nums):
+    start = 0
+    mid = 0 
+    end = len(nums)-1
+
+    while mid<=end:
+        if nums[mid]==0:
+            nums[start],nums[mid] = nums[mid],nums[start]
+            start+=1
+            mid+=1
+        elif nums[mid]==1:
+            mid+=1
+        elif nums[mid]==2:
+            nums[mid],nums[end] = nums[end],nums[mid]
+            end-=1
+
+nums = []
+n = int(input("Enter the Size : "))
+for i in range(n):
+    element = int(input("Enter the Data : "))
+    nums.append(element)
+
+sort_0_1_2(nums)
+print("After Sorting : ",nums)
