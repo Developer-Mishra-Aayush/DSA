a911a5c39dd28dc70fca2acbd301f6ddfaa4e03e
Aayush Mishra <ayush28014@gmail.com>
2025-08-19 11:37:22 +0530

    Binary Search

---
+++ Searching & Sorting/binary_search.py
@@ -0,0 +1,17 @@
+def binary_search(nums,target):
+    start = 0
+    end = len(nums)-1
+    while start<=end:
+        mid = start + (end - start)//2
+        if nums[mid]==target:
+            return True
+        elif nums[mid]>target:
+            end = mid -1
+        else:
+            start = mid + 1
+    return False
+
+nums = [2,8,10,20,30,34,46,78]
+target = 30
+ans = binary_search(nums,target)
+print("Target Found : ",ans)
