f287c0bee0453ebedc0dc0a67e74bc1a8c37b1e7
Aayush Mishra <ayush28014@gmail.com>
2025-08-21 10:54:01 +0530

    Reverse Letter only in String

---
+++ char Arrays & Strings/reverse_letters.py
@@ -0,0 +1,23 @@
+def isNotLetter(s):
+    if (s>='a' and s<='z') or (s>='A' and s<='Z'):
+        return False
+    return True
+
+def reverseLetter(s):
+    s = list(s)
+    start = 0
+    end = len(s)-1
+    while start<=end:
+        while isNotLetter(s[start]):
+            start+=1
+        while isNotLetter(s[end]):
+            end-=1
+        if start<=end:
+            s[start],s[end] = s[end],s[start]
+            start+=1
+            end-=1
+    return ''.join(s)
+
+s = input("Enter the String :")
+ans = reverseLetter(s)
+print("After Reversing Only Letters : ",ans)
