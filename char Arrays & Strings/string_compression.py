09befddb188ebda7f89b6308a27be92cc798b954
Aayush Mishra <ayush28014@gmail.com>
2025-08-21 15:18:27 +0530

    New Code Added Successfully

---
+++ char Arrays & Strings/string_compression.py
@@ -0,0 +1,20 @@
+def compress(chars):
+    dict = {}
+    for i in chars:
+        if i in dict:
+            dict[i]+=1
+        else:
+            dict[i] = 1
+    ans = ""
+    for i in dict:
+        ans+=i
+        if dict[i]!=1:
+            ans+=str(dict[i])
+    return list(ans)
+
+
+
+chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+# chars = ["a","a","b","b","c","c","c"]
+ans = compress(chars)
+print("Compress Ans : ",ans)
