551b85c0059a59c75d216e39af29d1af6cb5d9fa
Aayush Mishra <ayush28014@gmail.com>
2025-09-12 16:33:17 +0530

    New Code Added Successfully

---
+++ Generic & Binary Trees/duplicate_subtree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def __init__(self):
+        self.dict = {}
+        self.ans = []
+        
+    def preorder(self,root):
+        if root is None:
+            return 'N'
+        
+        N = str(root.val)
+        L = self.preorder(root.left)
+        R = self.preorder(root.right)
+        NLR = N + ',' + L + ',' + R
+        if NLR in self.dict:
+            if self.dict[NLR]==1:
+                self.ans.append(root)
+            self.dict[NLR]+=1
+        else:
+            self.dict[NLR] = 1
+
+        return NLR         
+
+    def findDuplicateSubtrees(self,root):
+        self.preorder(root)
+        return self.ans
